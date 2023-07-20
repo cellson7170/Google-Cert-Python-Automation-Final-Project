ac6d139292d5c977bd09ee9946cf7a3786ae9314
cellson7170 <chrismissed@gmail.com>
2023-07-19 17:18:33 -0700

    Add pdf generation

--- Course6/Project4 - Update Catalog/run.py
+++ Course6/Project4 - Update Catalog/run.py
@@ -2,5 +2,22 @@
 
 import os
 import requests
+import re
 
-files = os.listdir('')
+url = "http://[linux-instance-external-IP]/fruits"
+cwd = os.getcwd() + "/Course6/Project4 - Update Catalog/supplier-data/descriptions/"
+files = os.listdir(cwd)
+
+for item in files:
+    file = os.path.join(cwd, item)
+    with open(file, "r") as desc:
+        f, e = os.path.splitext(item)
+        image_name = f + ".jpg"
+        name, weight, description = desc.readlines()
+        weight = re.sub("[^0-9.\-]", "", weight)
+        dictionary = {"name":name.rstrip('\n'),
+                        "weight":int(weight),
+                        "description":description.rstrip('\n'),
+                        "image_name":image_name}
+    response = requests.post(url, data=dictionary)
+    print(response.status_code)
